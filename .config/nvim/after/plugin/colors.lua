d31af4815e62dc1c033c636ff51e290ae4a8148d
LuchoArch <luchoperronecontacto@gmail.com>
2024-07-28 07:59:50 -0300

    commented out the nvim color error

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,10 +1,6 @@
-let color = vim.cmd('source ~/.cache/wal/colors-wal.vim')
-
+-- let color = vim.cmd('source ~/.cache/wal/colors-wal.vim')
 
 function Colorize(color)
-
-
-	
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none" })
 end
